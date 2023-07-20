1c42898883d55ccc9ce2ba3d6951e2d1e5c896c0
Kodemon <hi@kodemon.net>
2023-07-20 16:38:07 +0900

    chore(blocks): return block details for latest blocks

--- src/Methods/Blockchain/GetLatestBlocks.ts
+++ src/Methods/Blockchain/GetLatestBlocks.ts
@@ -5,13 +5,15 @@ import { rpc } from "../../Services/Bitcoin";
 
 export const getLatestBlocks = method({
   params: Schema({
-    limit: number.optional(),
+    limit: number.lte(20).optional(),
   }),
-  handler: async ({ limit = 20 }) => {
+  handler: async ({ limit = 10 }) => {
     const blockCount = await rpc.blockchain.getBlockCount();
     const blocks = [];
     for (let i = 0; i < limit; i++) {
-      const block = await rpc.blockchain.getBlockHash(blockCount - i);
+      const hash = await rpc.blockchain.getBlockHash(blockCount - i);
+      const block = await rpc.blockchain.getBlock(hash);
+      delete (block as any).tx;
       blocks.push(block);
     }
     return blocks;
