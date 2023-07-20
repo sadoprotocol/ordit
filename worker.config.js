0918dd50805386c4efd682c9ae42520ad56585da
Kodemon <hi@kodemon.net>
2023-07-20 19:34:48 +0900

    chore(ord): use intervals from dotenv

--- worker.config.js
+++ worker.config.js
@@ -1,3 +1,7 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 module.exports = {
   apps: [
     {
@@ -5,7 +9,7 @@ module.exports = {
       script: "npm",
       args: "run ord:indexer",
       instances: 1,
-      cron_restart: "0/10 * * * *",
+      cron_restart: `0/${process.env.ORD_INDEXER_INTERVAL} * * * *`,
       exec_mode: "fork",
       watch: false,
       autorestart: false,
@@ -19,7 +23,7 @@ module.exports = {
       args: "run ord:snapshot",
       instances: 1,
       exec_mode: "fork",
-      cron_restart: "0 0/3 * * *",
+      cron_restart: `0 0/${process.env.ORD_SNAPSHOT_INTERVAL} * * *`,
       watch: false,
       autorestart: false,
       env: {
