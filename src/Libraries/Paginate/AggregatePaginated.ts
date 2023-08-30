f6a0e13b027d74f0df5d2ac1ba4851063d408e60
Kodemon <hi@kodemon.net>
2023-08-31 00:19:47 +0900

    fix(paginate): fix aggregate pagination function

--- src/Libraries/Paginate/AggregatePaginated.ts
+++ src/Libraries/Paginate/AggregatePaginated.ts
@@ -3,45 +3,23 @@ import { type Collection } from "mongodb";
 import { BaseDocument, Sort } from "./Types";
 import { buildCursor, buildQueryFromCursor, encodeCursor, normalizeDirectionParams } from "./Utilities";
 
-export type AggregatePaginatedParams = {
-  first?: number | null;
-  after?: string | null;
-  last?: number | null;
-  skip?: number | null;
-  before?: string | null;
-  pipeline: Array<{ [key: string]: any }>;
-  sort?: Sort;
-};
-
-export type AggregatePaginatedResult<TDocument> = {
-  edges: Array<{ cursor: string; node: TDocument }>;
-  pageInfo: {
-    count: number;
-    totalCount: number;
-    startCursor: string | null;
-    endCursor: string | null;
-    hasPreviousPage: boolean;
-    hasNextPage: boolean;
-  };
-};
-
 export const aggregatePaginated = async <TDocument extends BaseDocument>(
   collection: Collection,
-  { first, after, last, before, skip, pipeline, sort: originalSort = {} }: AggregatePaginatedParams
+  { limit, prev, next, skip, pipeline, sort: originalSort = {} }: AggregatePaginatedParams
 ): Promise<AggregatePaginatedResult<TDocument>> => {
-  const { limit, cursor, sort, paginatingBackwards } = normalizeDirectionParams({
-    first,
-    after,
-    last,
-    before,
+  const {
+    limit: lmt,
+    cursor,
+    sort,
+    reverse,
+  } = normalizeDirectionParams({
+    limit,
+    prev,
+    next,
     sort: originalSort,
   });
 
-  const documents = (await collection.aggregate([...pipeline, { $count: "countDocs" }])) as unknown as {
-    countDocs: number;
-  };
-
-  const allDocuments = (await collection
+  const result = (await collection
     .aggregate([
       ...pipeline,
       // When we receive a cursor, we must make sure only results after
@@ -51,34 +29,51 @@ export const aggregatePaginated = async <TDocument extends BaseDocument>(
       { $skip: skip ?? 0 },
       { $sort: sort },
       // Get 1 extra document to know if there's more after what was requested
-      { $limit: limit + 1 },
+      { $limit: lmt + 1 },
     ])
     .toArray()) as TDocument[];
 
   // Check whether the extra document mentioned above exists
-  const extraDocument = allDocuments[limit];
+  const extraDocument = result[lmt];
   const hasMore = Boolean(extraDocument);
 
   // Build an array without the extra document
-  const desiredDocuments = allDocuments.slice(0, limit);
-  if (paginatingBackwards) {
-    desiredDocuments.reverse();
+  const documents = result.slice(0, lmt) as any;
+  if (reverse) {
+    documents.reverse();
   }
 
-  const edges = desiredDocuments.map((document) => ({
-    cursor: encodeCursor(buildCursor(document, sort)),
-    node: document,
-  }));
+  for (const document of documents) {
+    document.$cursor = encodeCursor(buildCursor(document, sort));
+  }
+
+  const hasPreviousPage = reverse ? hasMore : Boolean(next);
+  const hasNextPage = reverse ? Boolean(prev) : hasMore;
 
   return {
-    edges,
-    pageInfo: {
-      count: desiredDocuments.length,
-      totalCount: documents.countDocs || 0,
-      startCursor: edges[0]?.cursor ?? null,
-      endCursor: edges[edges.length - 1]?.cursor ?? null,
-      hasPreviousPage: paginatingBackwards ? hasMore : Boolean(after),
-      hasNextPage: paginatingBackwards ? Boolean(before) : hasMore,
+    documents,
+    pagination: {
+      count: documents.length,
+      prev: hasPreviousPage ? documents[0]?.$cursor ?? null : null,
+      next: hasNextPage ? documents[documents.length - 1]?.$cursor ?? null : null,
     },
+  };
+};
+
+export type AggregatePaginatedParams = {
+  limit?: number;
+  prev?: string;
+  next?: string;
+  skip?: number;
+  pipeline: Array<{ [key: string]: any }>;
+  sort?: Sort;
+};
+
+export type AggregatePaginatedResult<T> = {
+  documents: Array<T & { $cursor: string }>;
+  pagination: {
+    count: number;
+    prev: string | null;
+    next: string | null;
   };
 };
