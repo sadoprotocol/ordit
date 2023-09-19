1dbf297907e23e1c11c7ae9ed5ea11d5887bca77
Kodemon <hi@kodemon.net>
2023-09-19 15:10:21 +0900

    fix(info): format to json

--- src/Methods/GetInfo.ts
+++ src/Methods/GetInfo.ts
@@ -5,12 +5,12 @@ import { config } from "../Config";
 
 export default method({
   handler: async () => {
-    const response = await fetch(`http://${config.worker.host}:${config.worker.port}/health`);
-    if (response.status !== 200) {
-      return {
-        status: "down",
-      };
-    }
-    return response;
+    return fetch(`http://${config.worker.host}:${config.worker.port}/health`)
+      .then((response) => response.json())
+      .catch(() => {
+        return {
+          status: "down",
+        };
+      });
   },
 });
