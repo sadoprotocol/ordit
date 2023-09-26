b346996453c004853198f8ef7c60a0692420ecbc
Kodemon <hi@kodemon.net>
2023-09-26 18:30:40 +0900

    chore(ordinals): add address lookup

--- src/Methods/Ordinals/GetOrdinals.ts
+++ src/Methods/Ordinals/GetOrdinals.ts
@@ -1,13 +1,26 @@
 import { method } from "@valkyr/api";
 import Schema, { string } from "computed-types";
 
+import { db } from "../../Database";
 import { ord } from "../../Services/Ord";
 
 export default method({
   params: Schema({
-    location: string,
+    address: string.optional(),
+    location: string.optional(),
   }),
-  handler: async ({ location }) => {
-    return ord.getOrdinals(location);
+  handler: async ({ address, location }) => {
+    if (location) {
+      return ord.getOrdinals(location);
+    }
+    if (address) {
+      const ordinals = [];
+      const outputs = await db.outputs.find({ addresses: address, vin: { $exists: false } });
+      for (const output of outputs) {
+        ordinals.push(...(await ord.getOrdinals(`${output.vout.txid}:${output.vout.n}`)));
+      }
+      return ordinals;
+    }
+    return [];
   },
 });
