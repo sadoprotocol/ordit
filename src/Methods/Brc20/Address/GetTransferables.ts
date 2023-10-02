80f7fb3942a3baa8b4e3ff2a2cd13f66cb735a5c
Kodemon <hi@kodemon.net>
2023-10-02 18:28:12 +0900

    fix(brc): add pagination to transferables

--- src/Methods/Brc20/Address/GetTransferables.ts
+++ src/Methods/Brc20/Address/GetTransferables.ts
@@ -4,18 +4,23 @@ import { Filter } from "mongodb";
 
 import { db } from "../../../Database";
 import { TokenTransfer } from "../../../Database/Brc20/Transfers/Collection";
-import { stripMongoIdFromMany } from "../../../Services/Mongo";
+import { schema } from "../../../Libraries/Schema";
 
 export default method({
   params: Schema({
     address: string,
     tick: string.optional(),
+    pagination: schema.pagination.optional(),
   }),
-  handler: async ({ address, tick }) => {
+  handler: async ({ address, tick, pagination }) => {
     const filter: Filter<TokenTransfer> = { "from.address": address, to: null };
     if (tick) {
       filter.slug = tick.toLowerCase();
     }
-    return db.brc20.transfers.find(filter).then(stripMongoIdFromMany);
+    const result = await db.brc20.transfers.findPaginated({ ...pagination, filter });
+    return {
+      transferables: result.documents,
+      pagination: result.pagination,
+    };
   },
 });
