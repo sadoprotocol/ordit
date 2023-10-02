3c0c0c928c46212f97e71c2b878f866043d83f0f
Kodemon <hi@kodemon.net>
2023-10-02 19:15:56 +0900

    feat(brc): add token include option to transferables

--- src/Methods/Brc20/Address/GetTransferables.ts
+++ src/Methods/Brc20/Address/GetTransferables.ts
@@ -3,6 +3,7 @@ import Schema, { string } from "computed-types";
 import { Filter } from "mongodb";
 
 import { db } from "../../../Database";
+import { Token } from "../../../Database/Brc20/Tokens/Collection";
 import { TokenTransfer } from "../../../Database/Brc20/Transfers/Collection";
 import { schema } from "../../../Libraries/Schema";
 
@@ -10,17 +11,45 @@ export default method({
   params: Schema({
     address: string,
     tick: string.optional(),
+    include: schema.include("token"),
     pagination: schema.pagination.optional(),
   }),
-  handler: async ({ address, tick, pagination }) => {
+  handler: async ({ address, tick, include, pagination }) => {
     const filter: Filter<TokenTransfer> = { "from.address": address, to: null };
     if (tick) {
       filter.slug = tick.toLowerCase();
     }
     const result = await db.brc20.transfers.findPaginated({ ...pagination, filter });
+    if (include?.includes("token")) {
+      result.documents = await includeTokenData(result.documents);
+    }
     return {
       transferables: result.documents,
       pagination: result.pagination,
     };
   },
 });
+
+async function includeTokenData(transfers: TokenTransfer[]): Promise<WithTokenData[]> {
+  const data = await db.brc20.tokens.find({ slug: { $in: transfers.map((transfer) => transfer.slug) } });
+  return transfers.map((transfer) => {
+    const token = data.find((token) => token.slug === transfer.slug);
+    if (token) {
+      return {
+        ...transfer,
+        token: {
+          inscription: token.inscription,
+          max: token.max,
+          amount: token.amount,
+          limit: token.limit,
+          decimal: token.decimal,
+          creator: token.creator,
+          timestamp: token.timestamp,
+        },
+      };
+    }
+    return token;
+  }) as WithTokenData[];
+}
+
+type WithTokenData = TokenTransfer & { token: Token; $cursor: string };
