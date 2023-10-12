983307c30fdee831d06b55c04076253665b91a7e
Kodemon <hi@kodemon.net>
2023-10-12 14:10:19 +0900

    feat(ordinal): add batch inscription method

---
+++ src/Methods/Ordinals/GetInscriptionsByIds.ts
@@ -0,0 +1,20 @@
+import { method } from "@valkyr/api";
+import Schema, { array, string } from "computed-types";
+
+import { config } from "~Config";
+import { db } from "~Database";
+
+export default method({
+  params: Schema({
+    ids: array.of(string),
+  }),
+  handler: async ({ ids }) => {
+    return db.inscriptions.find({ id: { $in: ids } }).then((inscriptions) => {
+      return inscriptions.map((inscription) => {
+        delete (inscription as any)._id;
+        inscription.mediaContent = `${config.api.uri}/content/${inscription.id}`;
+        return inscription;
+      });
+    });
+  },
+});
