ee5573b3ba8409955c676141c2f6a9f2835e7fc5
Kodemon <hi@kodemon.net>
2023-10-16 14:22:24 +0900

    chore(clean): remove unused files

--- src/Methods/Sado/Parse/Orders.ts
+++
@@ -1,9 +0,0 @@
-import { method } from "@valkyr/api";
-
-import { resolve } from "../../../Workers/Sado/Resolve";
-
-export default method({
-  handler: async () => {
-    return resolve();
-  },
-});
