23dbd94b0fe76df4ad744185a05e35637727631b
Kodemon <hi@kodemon.net>
2023-10-18 15:39:55 +0900

    feat(txs): add batch confirmations method

---
+++ src/Methods/Transactions/GetConfirmations.ts
@@ -0,0 +1,31 @@
+import { method } from "@valkyr/api";
+import Schema, { array, string } from "computed-types";
+
+import { rpc } from "~Services/Bitcoin";
+
+export default method({
+  params: Schema({
+    ids: array.of(string),
+  }),
+  handler: async ({ ids }) => {
+    const confirmations: Confirmation[] = [];
+    for (const id of ids) {
+      const transaction = await rpc.transactions.getRawTransaction(id, true);
+      if (!transaction) {
+        continue;
+      }
+      confirmations.push({
+        txid: transaction.txid,
+        confirmations: transaction.confirmations ?? -1,
+        timestamp: transaction.time || transaction.locktime,
+      });
+    }
+    return confirmations;
+  },
+});
+
+type Confirmation = {
+  txid: string;
+  confirmations: number;
+  timestamp: number;
+};
