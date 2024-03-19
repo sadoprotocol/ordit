87602c6dd9d825dc066937f38090379d2009ea9c
KevinK <kevin.kihata@gmail.com>
2024-03-19 16:32:01 +0800

    update param naming

--- src/Methods/Address/IsHolder.ts
+++ src/Methods/Address/IsHolder.ts
@@ -4,15 +4,15 @@ import Schema, { array, string } from "computed-types";
 import { db } from "../../Database";
 
 /**
- * Returns true if the address owns any one of the inscriptions from inscriptionList
+ * Returns true if the address owns any one of the inscriptions from inscriptionIds
  */
 export default method({
     params: Schema({
         address: string,
-        inscriptionList: array.of(string),
+        inscriptionIds: array.of(string),
     }),
-    handler: async ({ address, inscriptionList}) => {
-        const result = await db.inscriptions.findOne({owner: address, id: {$in: inscriptionList}});
+    handler: async ({ address, inscriptionIds}) => {
+        const result = await db.inscriptions.findOne({owner: address, id: {$in: inscriptionIds}});
         return !!result;
     },
 });
