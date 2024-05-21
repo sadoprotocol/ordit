1b525f197fbfd6d03d2ec1eefdf195b432fc6bc4
Daniel <daniel@adip.me>
2024-05-21 17:29:22 +0800

    fix(constants): fix epoch block numbers to match first inscription height on Ord

--- src/Libraries/Inscriptions/Constants.ts
+++ src/Libraries/Inscriptions/Constants.ts
@@ -2,7 +2,7 @@ import { config } from "../../Config";
 
 // prettier-ignore
 export const INSCRIPTION_EPOCH_BLOCK =
-  config.network === "mainnet" ? 767_429 :
-  config.network === "testnet" ? 2_413_342 :
-  config.network === "signet" ? 112_401 :
+  config.network === "mainnet" ? 767_430 :
+  config.network === "testnet" ? 2_413_343 :
+  config.network === "signet" ? 112_402 :
   0;
