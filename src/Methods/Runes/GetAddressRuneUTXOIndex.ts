271061dcb8760a867d9df4854e4b553014712bdb
dcorral <hi@dcorral.com>
2024-08-19 13:30:04 +0100

    Add pagination to GetAddressRuneUTXOIndex endpoint

--- src/Methods/Runes/GetAddressRuneUTXOIndex.ts
+++ src/Methods/Runes/GetAddressRuneUTXOIndex.ts
@@ -1,19 +1,25 @@
-import { method, NotFoundError } from "@valkyr/api";
+import { method } from "@valkyr/api";
 import Schema, { string } from "computed-types";
 import { RuneUtxoBalance } from "runestone-lib";
 
 import { runes } from "~Database/Runes";
+import { FindPaginatedParams } from "~Libraries/Paginate";
+
+import { schema } from "../../Libraries/Schema";
 
 export default method({
   params: Schema({
     address: string,
     runeTicker: string,
+    pagination: schema.pagination.optional(),
   }),
-  handler: async ({ address, runeTicker }): Promise<RuneUtxoBalance[]> => {
-    const balances = await runes.addressRunesUTXOs(address, runeTicker);
-    if (balances === null) {
-      throw new NotFoundError("Address not found");
-    }
-    return balances;
+  handler: async ({ address, runeTicker, pagination = {} }) => {
+    const params: FindPaginatedParams<RuneUtxoBalance> = { ...pagination, filter: { address, runeTicker } };
+    const balances = await runes.addressRunesUTXOs(params);
+
+    return {
+      balances: balances.documents,
+      pagination: balances.pagination,
+    };
   },
 });
