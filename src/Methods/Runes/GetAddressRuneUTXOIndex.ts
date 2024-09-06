7d482b026ef0c953debdc72bbc443d180fb28612
dcorral <hi@dcorral.com>
2024-09-06 09:20:33 +0100

    Make runeTicker optional and add includeSpent param to GetAddressRuneUTXOIndex

--- src/Methods/Runes/GetAddressRuneUTXOIndex.ts
+++ src/Methods/Runes/GetAddressRuneUTXOIndex.ts
@@ -1,5 +1,6 @@
 import { method } from "@valkyr/api";
-import Schema, { string } from "computed-types";
+import Schema, { boolean, string } from "computed-types";
+import { Filter } from "mongodb";
 import { RuneUtxoBalance } from "runestone-lib";
 
 import { runes } from "~Database/Runes";
@@ -10,16 +11,25 @@ import { schema } from "../../Libraries/Schema";
 export default method({
   params: Schema({
     address: string,
-    runeTicker: string,
+    runeTicker: string.optional(),
     pagination: schema.pagination.optional(),
     sort: schema.sort,
+    includeSpent: boolean.optional(),
   }),
-  handler: async ({ address, runeTicker, pagination = {}, sort }) => {
+  handler: async ({ address, runeTicker, pagination = {}, sort, includeSpent = false }) => {
     pagination.limit ??= 50;
     sort ??= { _id: "asc" };
+
+    const filter: Filter<RuneUtxoBalance> = { address };
+    if (runeTicker) {
+      filter.runeTicker = runeTicker;
+    }
+    if (!includeSpent) {
+      filter.spentTxid = { $exists: false };
+    }
     const params: FindPaginatedParams<RuneUtxoBalance> = {
       ...pagination,
-      filter: { address, runeTicker },
+      filter,
       sort,
       cursorInfo: false,
     };
