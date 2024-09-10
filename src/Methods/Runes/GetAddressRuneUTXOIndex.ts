eee74b8184be40d9ae192c943156807f54a77de9
Diego <hi@dcorral.com>
2024-09-10 18:03:25 +0100

    fix(api): make runeTicker optional and add includeSpent param (#83)
    * Make runeTicker optional and add includeSpent param to GetAddressRuneUTXOIndex
    
    * Temporary fix to return satValue on UTXOs

--- src/Methods/Runes/GetAddressRuneUTXOIndex.ts
+++ src/Methods/Runes/GetAddressRuneUTXOIndex.ts
@@ -1,7 +1,10 @@
 import { method } from "@valkyr/api";
-import Schema, { string } from "computed-types";
+import Schema, { boolean, string } from "computed-types";
+import { Filter } from "mongodb";
 import { RuneUtxoBalance } from "runestone-lib";
 
+import { db } from "~Database";
+import { OutputDocument } from "~Database/Output";
 import { runes } from "~Database/Runes";
 import { FindPaginatedParams } from "~Libraries/Paginate";
 
@@ -10,21 +13,42 @@ import { schema } from "../../Libraries/Schema";
 export default method({
   params: Schema({
     address: string,
-    runeTicker: string,
+    runeTicker: string.optional(),
     pagination: schema.pagination.optional(),
     sort: schema.sort,
+    includeSpent: boolean.optional(),
+    includeSatValue: boolean.optional(),
   }),
-  handler: async ({ address, runeTicker, pagination = {}, sort }) => {
+  handler: async ({ address, runeTicker, pagination = {}, sort, includeSpent = false, includeSatValue = false }) => {
     pagination.limit ??= 50;
     sort ??= { _id: "asc" };
+
+    const filter: Filter<RuneUtxoBalance> = { address };
+    if (runeTicker) {
+      filter.runeTicker = runeTicker;
+    }
+    if (!includeSpent) {
+      filter.spentTxid = { $exists: false };
+    }
     const params: FindPaginatedParams<RuneUtxoBalance> = {
       ...pagination,
-      filter: { address, runeTicker },
+      filter,
       sort,
       cursorInfo: false,
     };
-    const balances = await runes.addressRunesUTXOs(params);
 
+    const balances = await runes.addressRunesUTXOs(params);
+    if (includeSatValue) {
+      await Promise.all(
+        balances.documents.map(async (balance: any) => {
+          const filter: Filter<OutputDocument> = { "vout.txid": balance.txid, "vout.n": balance.vout };
+          const vout = await db.outputs.findOne(filter);
+          if (vout?.value) {
+            balance.value = vout.value;
+          }
+        }),
+      );
+    }
     return {
       balances: balances.documents,
       pagination: balances.pagination,
