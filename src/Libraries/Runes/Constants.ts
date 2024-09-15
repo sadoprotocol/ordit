4c971c85c76ef7faf350c5f3a3c6c0e9ccf0eda3
dcorral <hi@dcorral.com>
2024-09-15 10:25:43 +0100

    Add fractal Network support

--- src/Libraries/Runes/Constants.ts
+++ src/Libraries/Runes/Constants.ts
@@ -4,7 +4,9 @@ export const RUNES_BLOCK =
   config.network === "mainnet"
     ? 840_000
     : config.network === "testnet"
-    ? 2520000
-    : config.network === "signet"
-    ? 188_714
-    : 0;
+      ? 2520000
+      : config.network === "signet"
+        ? 188_714
+        : config.network === "fractal"
+          ? 21_000
+          : 0;
